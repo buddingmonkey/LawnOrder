bc0b439488c3365e8628127a9693311a28d33d48
Andrew Eiche <eiche_andrew@bah.com>
2015-01-24 13:21:11 -0500

    more looping

---
+++ SmokeThatWeedGame/Assets/Scripts/WorldBound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldBound : MonoBehaviour {
+	Bounds b;
+
+	void Start(){
+		b = this.GetComponentInParent<Bounds>();;
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		b.OnTriggerEnter2D(other);
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+		b.OnTriggerStay2D(other);
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		Debug.Log("Collide");
+	}
+}
