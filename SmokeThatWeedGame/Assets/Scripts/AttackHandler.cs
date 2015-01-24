3d0ced6e88c4d85264fb5730303246ec51d12fe8
Levi Lansing <helives@gmail.com>
2015-01-24 16:05:52 -0500

    bullet damage, flower assets

--- SmokeThatWeedGame/Assets/Scripts/AttackHandler.cs
+++ SmokeThatWeedGame/Assets/Scripts/AttackHandler.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class AttackHandler : MonoBehaviour {
 	public float damage = 1;
+	public bool stopAfterHit = true;
 
 	
 	public void OnTriggerEnter2D(Collider2D collider) {
 		if (collider.tag.Equals("Enemy")) {
-			print (collider.tag);
 			var enemy = collider.GetComponentInParent<BaseEnemy>();
 			enemy.TakeDamage(damage, (collider.transform.position - transform.position).normalized);
+
+			if (stopAfterHit) {
+				Destroy (gameObject);
+			}
 		}
 	}
 }
