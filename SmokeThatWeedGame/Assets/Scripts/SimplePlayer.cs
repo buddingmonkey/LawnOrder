141a68750a3070dffa48cbba0f08a7254d3dc174
Andrew Eiche <eiche_andrew@bah.com>
2015-01-23 23:01:25 -0500

    Eiche commit 1

---
+++ SmokeThatWeedGame/Assets/Scripts/SimplePlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class SimplePlayer : MonoBehaviour {
+	public int playerNum;
+
+	private Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(InputManager.Devices);
+		body = this.rigidbody2D;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var inputDevice = InputManager.Devices[0];//(InputManager.Devices.Count > playerNum) ? InputManager.Devices[playerNum] : null;
+		Debug.Log(inputDevice.DPad.Vector);
+		body.AddForce(Vector2.right * Time.deltaTime * inputDevice.LeftStick.X);
+	}
+}
