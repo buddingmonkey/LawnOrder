7e780395b6a57b18d26eec27ef84b0408f5173f8
Andrew Eiche <eiche_andrew@bah.com>
2015-01-24 00:20:58 -0500

    adding prefabs

---
+++ SmokeThatWeedGame/Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	public enum Path{
+		linear,
+		sine,
+		spread
+	}
+
+	public Path path = Path.linear;
+	public float speed = 10f;
+	public Vector2 direction = Vector2.right;
+
+	private Transform t;
+
+	// Use this for initialization
+	void Start () {
+		t = this.transform;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		t.Translate(direction * speed * Time.deltaTime);
+	}
+}
---
+++ SmokeThatWeedGame/Assets/Scripts/Shooter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+	public Transform currentProjectile;
+	public Transform[] projectiles;
+
+	public Transform shootLocation;
+
+	// Use this for initialization
+	void Start () {
+		TESTMETHOD();
+	}
+
+	void TESTMETHOD(){
+		currentProjectile = projectiles[0];
+		InvokeRepeating("Shoot", .1f, .1f);
+	}
+
+	void Shoot(){
+		if (currentProjectile == null) return;
+		Instantiate (currentProjectile, shootLocation.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
