e5672c1ea82b75c243df63d5954d6e4183a8f90b
Andrew Eiche <eiche_andrew@bah.com>
2015-01-24 09:49:26 -0500

    Adding base level

--- SmokeThatWeedGame/Assets/Scripts/InputControls.cs
+++ SmokeThatWeedGame/Assets/Scripts/InputControls.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using InControl;
 
 public class InputControls : MonoBehaviour {
 
+
+	void Start(){
+	}
+
 	public bool Jumping() {
-		return Input.GetButton ("Jump");
+		return Input.GetButton ("Action1");
 	}
 
 	public float XAxis() {
