12e1e05bb7de9528e6b26d3aae150439422b6b38
Andrew Eiche <eiche_andrew@bah.com>
2015-01-23 20:50:35 -0500

    adding tile editor

---
+++ SmokeThatWeedGame/Assets/Tile Editor/Scripts/Class/TileMap.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TileEditor
+{
+    public class TileMap : MonoBehaviour
+    {
+        #region Public Fields
+        public Vector2 NewMapSize;
+        public Vector2 MapSize;
+        public Vector2 TileSize;
+        public string ResourceFoler;
+        public List<TileLayer> TileLayers;
+        public float PreviewSize;
+        public int SelectedLayerIndex;
+        public bool ShowEdit;
+        #endregion
+
+        #region Public Methods
+        public bool IsInTileMap(Vector2 pos)
+        {
+            float minX = transform.position.x;
+            float maxX = minX + MapSize.x * TileSize.x;
+            float minY = transform.position.y;
+            float maxY = minY + MapSize.y * TileSize.y;
+            return (pos.x >= minX && pos.x <= maxX && pos.y >= minY && pos.y <= maxY);
+        }
+
+        public Vector2 TransformPositionToGridIndex(Vector2 position)
+        {
+            Vector2 gridIndex = new Vector2(
+                   (int)((position.x - transform.position.x) / TileSize.x),
+                   (int)((position.y - transform.position.y) / TileSize.y)
+                );
+
+            return gridIndex;
+        }
+
+        public Vector2 ClampGridIndex(Vector2 gridIndex)
+        {
+            return new Vector2((int)Mathf.Clamp(gridIndex.x, 0, MapSize.x - 1), (int)Mathf.Clamp(gridIndex.y, 0, MapSize.y - 1));
+        }
+
+        public Vector2 GetGridIndexPosInWorldSpace(Vector2 gridIndex)
+        {
+            Vector2 pos = new Vector2(gridIndex.x * TileSize.x,
+                gridIndex.y * TileSize.y);
+            pos += new Vector2(0.5f * TileSize.x, 0.5f * TileSize.y);
+            pos += new Vector2(transform.position.x, transform.position.y);
+            return pos;
+        }
+
+        #endregion
+    }
+
+}
---
+++ SmokeThatWeedGame/Assets/Tile Editor/Scripts/Class/TileSet.cs
@@ -0,0 +1,16 @@
+﻿#if  UNITY_EDITOR
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TileEditor
+{
+    [System.Serializable]
+    public class TileSet
+    {
+        #region Public Fields
+        public List<TileData> TileList;
+        #endregion
+    }
+}
+#endif
