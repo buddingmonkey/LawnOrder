f5f3013fb0d3301d7482d389822c96ea0bcaf767
Levi Lansing <helives@gmail.com>
2015-01-24 01:16:53 -0500

    start of base enemy. game over panel placeholder prefab

--- SmokeThatWeedGame/Assets/Scripts/BaseEnemy.cs
+++ SmokeThatWeedGame/Assets/Scripts/BaseEnemy.cs
@@ -4,13 +4,17 @@ using System.Collections;
 public class BaseEnemy : MonoBehaviour {
 
 	public Sprite[] stages;
-	public int movementStage = 3;
-
+	public int movementStage = 2;
+	public float startingHealth = 5;
+	public float speed = 1;
 	public float growthRateSec;
+
 	float timeInStage;
 	int stage;
 	SpriteRenderer spriteRenderer;
+	new Transform transform;
 
+	float health;
 	float direction;
 
 	// Use this for initialization
@@ -18,6 +22,9 @@ public class BaseEnemy : MonoBehaviour {
 		stage = 0;
 		timeInStage = 0;
 		spriteRenderer = (SpriteRenderer)GetComponent<SpriteRenderer> ();
+		health = startingHealth;
+		transform = GetComponent<Transform> ();
+		direction = 1;
 	}
 	
 	// Update is called once per frame
@@ -28,5 +35,30 @@ public class BaseEnemy : MonoBehaviour {
 			timeInStage = 0;
 			spriteRenderer.sprite = stages[stage];
 		}
+
+//		if (stage >= movementStage) {
+			Move ();
+//		}
+	}
+
+	public void TakeDamage(float damage) {
+		health -= damage;
+		if (damage < 0) {
+			Die();
+		}
+	}
+
+	void Die() {
+		Destroy (gameObject);
+	}
+
+	public void Move() {
+		Vector2 position = new Vector2 (transform.position.x,transform.position.y);
+		Vector2 moveTo = position + Vector2.right * direction * Time.deltaTime;
+		RaycastHit2D hit = Physics2D.Raycast(moveTo - (Vector2.up*.5f), -Vector2.up, .5f);
+		if (hit.transform != null) {
+			Debug.DrawRay(hit.point,-Vector3.up*.5f,Color.red);
+		}
+
 	}
 }
