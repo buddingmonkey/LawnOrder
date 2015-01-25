39c2d3f0287f5da72c2c125c77c64c444179ee04
Wesley Boynton <wwboynton@gmail.com>
2015-01-25 08:37:21 -0500

    Added Music GameObject

---
+++ SmokeThatWeedGame/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip [] clips;
+	public int clipIndex;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<AudioSource> ().clip = clips[clipIndex];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
