be791c74fca80aae009fc1228865e6ff09f04f49
Andrew Eiche <eiche_andrew@bah.com>
2015-01-25 12:34:11 -0500

    MR WORLD WIDE

---
+++ SmokeThatWeedGame/Assets/Items/BugSpray.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BugSpray : Holdable {
+
+	// Use this for initialization
+	void Start () {
+		base.Start ();
+	}
+	
+	public override void GetHeld(CharacterMovement newHolder){
+		base.GetHeld(newHolder);
+		this.rigidbody2D.isKinematic = true;
+		var shooter = newHolder.GetComponent<Shooter>();
+		shooter.currentProjectile = shooter.projectiles[bulletID];
+		shooter.autoFire = false;
+		shooter.coolDown = .05f; // 1FRAME!
+	}
+	
+	public override void GetDropped(Vector2 impulse){
+		base.GetDropped(impulse);
+		this.rigidbody2D.isKinematic = false;
+	}
+	
+	// Update is called once per frame
+	new void Update () {
+		base.Update ();
+	}
+}
