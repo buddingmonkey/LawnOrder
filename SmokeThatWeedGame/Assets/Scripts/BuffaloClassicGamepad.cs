18ef06b14fca0231aec36882c1d2e8653945ba60
Andrew Eiche <eiche_andrew@bah.com>
2015-01-24 23:07:52 -0500

    Fixing the gamepad support

--- SmokeThatWeedGame/Assets/Scripts/BuffaloClassicGamepad.cs
+++ SmokeThatWeedGame/Assets/Scripts/BuffaloClassicGamepad.cs
@@ -1,118 +1,121 @@
 ﻿using UnityEngine;
 using System.Collections;
-using InControl;
 
-public class BuffaloClassicGamepad : UnityInputDeviceProfile {
-	
-	public BuffaloClassicGamepad()
-	{
-		Name = "Unknown Device ( USB,2-axis 8-button gamepad)";
-		Meta = "Buffalo Classic Gamepad on Mac";
-		
-		SupportedPlatforms = new[] {
-			"OS X"
-		};
-		
-		JoystickNames = new[] {
-			"Unknown Device ( USB,2-axis 8-button gamepad)"
-		};
-		
-		ButtonMappings = new[] {
-			new InputControlMapping {
-				Handle = "B",
-				Target = InputControlType.Action1,
-				Source = Button1
-			},
-			new InputControlMapping {
-				Handle = "A",
-				Target = InputControlType.Action2,
-				Source = Button0
-			},
-			new InputControlMapping {
-				Handle = "Y",
-				Target = InputControlType.Action3,
-				Source = Button3
-			},
-			new InputControlMapping {
-				Handle = "X",
-				Target = InputControlType.Action4,
-				Source = Button2
-			},
-			new InputControlMapping {
-				Handle = "Select",
-				Target = InputControlType.Back,
-				Source = Button6
-			},
-			new InputControlMapping {
-				Handle = "Start",
-				Target = InputControlType.Start,
-				Source = Button7
-			},
-			new InputControlMapping {
-				Handle = "Left Bumper",
-				Target = InputControlType.LeftBumper,
-				Source = Button4
-			},
-			new InputControlMapping {
-				Handle = "Right Bumper",
-				Target = InputControlType.RightBumper,
-				Source = Button5
-			},
-			new InputControlMapping {
-				Handle = "Left Trigger",
-				Target = InputControlType.LeftTrigger,
-				Source = Button4
-			},
-			new InputControlMapping {
-				Handle = "Right Trigger",
-				Target = InputControlType.RightTrigger,
-				Source = Button5
-			}
-		};
-		
-		AnalogMappings = new[] {
-			/*new InputControlMapping {
-				Handle = "Left Stick X",
-				Target = InputControlType.LeftStickX,
-				Source = Analog0
-			},
-			new InputControlMapping {
-				Handle = "Left Stick Y",
-				Target = InputControlType.LeftStickY,
-				Source = Analog1,
-				Invert = true
-			},*/
-			new InputControlMapping {
-				Handle = "DPad Left",
-				Target = InputControlType.DPadLeft,
-				Source = Analog0,
-				SourceRange = InputControlMapping.Range.Negative,
-				TargetRange = InputControlMapping.Range.Negative,
-				Invert = true
-			},
-			new InputControlMapping {
-				Handle = "DPad Right",
-				Target = InputControlType.DPadRight,
-				Source = Analog0,
-				SourceRange = InputControlMapping.Range.Positive,
-				TargetRange = InputControlMapping.Range.Positive
-			},
-			new InputControlMapping {
-				Handle = "DPad Up",
-				Target = InputControlType.DPadUp,
-				Source = Analog1,
-				SourceRange = InputControlMapping.Range.Negative,
-				TargetRange = InputControlMapping.Range.Negative,
-				Invert = true
-			},
-			new InputControlMapping {
-				Handle = "DPad Down",
-				Target = InputControlType.DPadDown,
-				Source = Analog1,
-				SourceRange = InputControlMapping.Range.Positive,
-				TargetRange = InputControlMapping.Range.Positive
-			}
-		};
+namespace InControl{
+	public class BuffaloClassicGamepad : UnityInputDeviceProfile {
+		[AutoDiscover]
+		public BuffaloClassicGamepad()
+		{
+			Name = " USB,2-axis 8-button gamepad";
+			Meta = "Buffalo Classic Gamepad on Mac";
+			
+			SupportedPlatforms = new[] {
+				"Windows",
+				"Mac",
+				"Linux"
+			};
+			
+			JoystickNames = new[] {
+				" USB,2-axis 8-button gamepad"
+			};
+			
+			ButtonMappings = new[] {
+				new InputControlMapping {
+					Handle = "B",
+					Target = InputControlType.Action1,
+					Source = Button1
+				},
+				new InputControlMapping {
+					Handle = "A",
+					Target = InputControlType.Action2,
+					Source = Button0
+				},
+				new InputControlMapping {
+					Handle = "Y",
+					Target = InputControlType.Action3,
+					Source = Button3
+				},
+				new InputControlMapping {
+					Handle = "X",
+					Target = InputControlType.Action4,
+					Source = Button2
+				},
+				new InputControlMapping {
+					Handle = "Select",
+					Target = InputControlType.Back,
+					Source = Button6
+				},
+				new InputControlMapping {
+					Handle = "Start",
+					Target = InputControlType.Start,
+					Source = Button7
+				},
+				new InputControlMapping {
+					Handle = "Left Bumper",
+					Target = InputControlType.LeftBumper,
+					Source = Button4
+				},
+				new InputControlMapping {
+					Handle = "Right Bumper",
+					Target = InputControlType.RightBumper,
+					Source = Button5
+				},
+				new InputControlMapping {
+					Handle = "Left Trigger",
+					Target = InputControlType.LeftTrigger,
+					Source = Button4
+				},
+				new InputControlMapping {
+					Handle = "Right Trigger",
+					Target = InputControlType.RightTrigger,
+					Source = Button5
+				}
+			};
+			
+			AnalogMappings = new[] {
+				new InputControlMapping {
+					Handle = "Left Stick X",
+					Target = InputControlType.LeftStickX,
+					Source = Analog0
+				},
+				new InputControlMapping {
+					Handle = "Left Stick Y",
+					Target = InputControlType.LeftStickY,
+					Source = Analog1,
+					Invert = true
+				},
+				new InputControlMapping {
+					Handle = "DPad Left",
+					Target = InputControlType.DPadLeft,
+					Source = Analog0,
+					SourceRange = InputControlMapping.Range.Negative,
+					TargetRange = InputControlMapping.Range.Negative,
+					Invert = true
+				},
+				new InputControlMapping {
+					Handle = "DPad Right",
+					Target = InputControlType.DPadRight,
+					Source = Analog0,
+					SourceRange = InputControlMapping.Range.Positive,
+					TargetRange = InputControlMapping.Range.Positive
+				},
+				new InputControlMapping {
+					Handle = "DPad Up",
+					Target = InputControlType.DPadUp,
+					Source = Analog1,
+					SourceRange = InputControlMapping.Range.Negative,
+					TargetRange = InputControlMapping.Range.Negative,
+					Invert = true
+				},
+				new InputControlMapping {
+					Handle = "DPad Down",
+					Target = InputControlType.DPadDown,
+					Source = Analog1,
+					SourceRange = InputControlMapping.Range.Positive,
+					TargetRange = InputControlMapping.Range.Positive
+				}
+			};
+		}
+
 	}
-
 }
